83f5f5a1268a57cf1bb8a0d102f6d591fc11184a
dng <dominicnzl@hotmail.com>
2021-02-05 15:11:58 +0100

    Replaced explicit getEnv() call with @Value annotations

--- src/main/java/ng/dominic/awsupload/config/AmazonConfig.java
+++ src/main/java/ng/dominic/awsupload/config/AmazonConfig.java
@@ -6,21 +6,21 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AmazonConfig {
 
-    /**
-     * My SuperSecretCredentials were added to .gitignore obviously...
-     *
-     * @return
-     */
+    @Value("${AWS_S3_ACCESSKEY}")
+    private String accesskey;
+
+    @Value("AWS_S3_SECRETKEY")
+    private String secretkey;
+
     @Bean
     public AmazonS3 s3() {
-        var accesskey = System.getenv().get("AWS_S3_ACCESSKEY");
-        var secretkey = System.getenv().get("AWS_S3_SECRETKEY");
         AWSCredentials awsCredentials = new BasicAWSCredentials(accesskey, secretkey);
         return AmazonS3ClientBuilder.standard()
                 .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
